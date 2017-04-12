1b2eeed1faf0f8a7e6bcf9f97fb3cbe72049060f
Matthew B. Jones <matthewbjones85@gmail.com>
2017-04-12 11:37:14 -0400

    (bundler) Upgrade bundler to latest Heroku version

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -12,9 +12,9 @@ require "language_pack/version"
 # base Ruby Language Pack. This is for any base ruby app.
 class LanguagePack::Ruby < LanguagePack::Base
   NAME                 = "ruby"
-  LIBYAML_VERSION      = "0.1.6"
+  LIBYAML_VERSION      = "0.1.7"
   LIBYAML_PATH         = "libyaml-#{LIBYAML_VERSION}"
-  BUNDLER_VERSION      = "1.7.12"
+  BUNDLER_VERSION      = "1.13.7"
   BUNDLER_GEM_PATH     = "bundler-#{BUNDLER_VERSION}"
   DEFAULT_RUBY_VERSION = "ruby-2.0.0"
   RBX_BASE_URL         = "http://binaries.rubini.us/heroku"
